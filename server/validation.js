a9d0a1f937c1f3a505e33c4e4fb236e14240637e
Eduardo Russo <russoedu@gmail.com>
2016-11-09 17:34:13 -0200

    [master] Validation test

--- server/validation.js
+++ server/validation.js
@@ -6,13 +6,13 @@ module.exports = {
    * validation finishes. The callback parameters are a Boolean, that responds
    * if it's valid and an Object with the response data
    */
-  validate: function(data, callback) {
-    var valid = false;
-    var response = {};
-    if (data.clientId !== 'olar') {
-      valid = true;
-    }
-		response = {
+  clientId: function(data, callback) {
+    var valid = true;
+    // var response = {};
+    // if (data.clientId !== 'olar') {
+    //   valid = true;
+    // }
+		var response = {
 			data: {
 				apiUrl: 'https://services.virtualkar.com.br/rest/',
 				test: data
