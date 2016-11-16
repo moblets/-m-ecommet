4bf17f2717f4eae2fe1c034be91269c8cf170c95
russoedu <russoedu@gmail.com>
2016-11-16 11:10:57 -0200

    [master] Updated form and validation

--- server/validation.js
+++ server/validation.js
@@ -1,33 +1,89 @@
+const https = require('https');
+
 module.exports = {
   /**
    * Validate a given ID and saves the url
-   * @param {String}  id The client id
+   * @param {Object}  data The moblet data
    * @param {Function} callback The callback that will be called when the
    * validation finishes. The callback parameters are a Boolean, that responds
    * if it's valid and an Object with the response data
    */
   moblet: function(data, callback) {
-    var valid = true;
-		// Validation debug
-		// In the future, test if it's a valid client ID
-    if (data.clientId == 'olar') {
+    console.log(data.clientUrl);
+    var valid = false;
+    var response = {};
+    var options = {
+      hostname: 'services.' + data.clientUrl,
+      port: 443,
+      path: '/rest/common/conf/store',
+      method: 'GET'
+    };
+
+    var req = https.request(options, res => {
+      var body = '';
+      console.log(res.statusCode);
+
+      // URL not found = response !== 200
+      if (res.statusCode !== 200) {
+        valid = false;
+        response = {
+          errors: {
+            clientUrl: 'url_error'
+          }
+        };
+      }
+
+      // URL found. Capturing stream data on body
+      res.on('data', chunk => {
+        body += chunk;
+      });
+
+      // URL found. response finished
+      res.on('end', function() {
+        body = JSON.parse(body);
+        console.log(body);
+        console.log(data.clientId);
+        console.log(body.clientHash);
+        if (body.clientHash === undefined) {
+          // Client HASH not found on response
+          valid = false;
+          response = {
+            errors: {
+              clientUrl: 'url_error'
+            }
+          };
+        } else if (body.clientHash === data.clientId) {
+          // Client HASH found on response AND match
+          valid = true;
+          response = {
+            data: {
+              apiUrl: 'https://services.' + data.clientUrl + '/rest/'
+            }
+          };
+        } else {
+          // Client HASH found on response but doesn't match
+          valid = false;
+          response = {
+            errors: {
+              clientId: 'url_hash_check'
+            }
+          };
+        }
+        callback(valid, response);
+      });
+    });
+
+    req.end();
+
+    req.on('error', error => {
+      console.error(error);
       valid = false;
-    }
-		// Get apiUrl from the API if possible
-		var response = {
-			data: {
-				apiUrl: 'https://services.virtualkar.com.br/rest/',
-			}
-		};
-		// if not possible, add to the form.json
-		// ,
-		// {
-    //   "name": "baseApiUrl",
-    //   "type": "text",
-    //   "required": true,
-    //   "min-length": 13,
-    //   "max-length": 120
-    // }
-    callback(valid, response);
+      response = {
+        errors: {
+          clientUrl: 'url_error'
+        }
+      };
+      callback(valid, response);
+    });
   }
 };
