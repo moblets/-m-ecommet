ec6aaa13d79c2b346a2d7077e2418e665659c7a4
russoedu <russoedu@gmail.com>
2016-11-16 15:48:19 -0200

    [master] Layout

--- moblet/m-becommerce.js
+++ moblet/m-becommerce.js
@@ -91,126 +91,54 @@ module.exports = {
     // var dataLoadOptions;
     // var apiUrl = '';
     var controller = {
-    //   getHeight: function() {
-    //     var maxHeight = 0;
-    //     for (var i = 0; i < $scope.slider.length; i++) {
-    //       var tmpImg = new Image();
-    //       tmpImg.src = $scope.slider[i].image;
-    //       tmpImg.onload = function() {
-    //         var proportion = window.screen.width / tmpImg.width;
-    //         var height = tmpImg.height * proportion;
-    //         maxHeight = height > maxHeight ? height : maxHeight;
-    //         $timeout(function() {
-    //           $scope.slider.height = maxHeight + 10 + 'px';
-    //         }, 10);
-    //       };
-    //     }
-    //   },
-    //   startSlider: function() {
-    //     function getNext(value) {
-    //       if (value < loopCount) {
-    //         return value + 1;
-    //       } else {
-    //         return 0;
-    //       }
-    //     }
-    //
-    //     var loopCount = $scope.slider.length - 1;
-    //     var current = 0;
-    //     var previous = loopCount;
-    //     var next = getNext(current);
-    //
-    //     $scope.slider[current].currentSlide = ' current-slide';
-    //     $scope.slider[next].nextSlide = ' next-slide';
-    //
-    //     $interval(function() {
-    //       previous = current;
-    //       current = getNext(current);
-    //       next = getNext(current);
-    //
-    //       $scope.slider[current].currentSlide = ' current-slide';
-    //       $scope.slider[current].nextSlide = '';
-    //
-    //       $scope.slider[previous].currentSlide = '';
-    //
-    //       $scope.slider[next].nextSlide = ' next-slide';
-    //     }, 3000);
-    //     ecommet.getHeight();
-    //   },
-    //   getData: function(url, callback) {
-    //     $http.get(apiUrl + url)
-    //       .then(
-    //         function(response) {
-    //           callback(false, response);
-    //         },
-    //         function(error) {
-    //           console.log(error);
-    //           calback(true);
-    //         }
-    //       );
-    //   },
-    //   /**
-    //    * Set the view and update the needed parameters
-    //    * @param  {object} data Data received from Moblets backend
-    //    * @param  {boolean} more If called by "more" function, it will add the
-    //    * data to the items array
-    //    */
-    //   /**
-    //    * Check if the view is showing a detail or the list. The function checks
-    //    * if $stateParams.detail is set.
-    //    * @return {boolean} True if the view must show a detail.
-    //    */
-    //   isDetail: function() {
-    //     return $stateParams.detail !== "";
-    //   },
-    //   /**
-    //    * Show the detail getting the index from $stateParams.detail. Set "item"
-    //    * to the selected detail
-    //    */
-    //   // showDetail: function(detailIndex) {
-    //   //   if (isDefined($stateParams.detail) && $stateParams.detail !== "") {
-    //   //     var itemIndex = _.findIndex($scope.items, function(item) {
-    //   //       return item.id.toString() === $stateParams.detail;
-    //   //     });
-    //   //     if (itemIndex === -1) {
-    //   //       dataLoadOptions = {
-    //   //         offset: $scope.items === undefined ? 0 : $scope.items.length,
-    //   //         items: 25,
-    //   //         cache: false
-    //   //       };
-    //   //       list.load(false, function() {
-    //   //         list.showDetail();
-    //   //       });
-    //   //     } else {
-    //   //       $scope.detail = $scope.items[itemIndex];
-    //   //     }
-    //   //   } else if (isDefined(detailIndex)) {
-    //   //     $scope.detail = $scope.items[detailIndex];
-    //   //   }
-    //   // },
-    //   /**
-    //    * Load data from the Moblets backend:
-    //    * - show the page loader if it's called by init (sets showLoader to true)
-    //    * - Use $mDataLoader.load to get the moblet data from Moblets backend.
-    //    * 	 The parameters passed to $mDataLoader.load are:
-    //    * 	 - $scope.moblet - the moblet created in the init function
-    //    * 	 - false - A boolean that sets if you want to load data from the
-    //    * 	   device storage or from the Moblets API
-    //    * 	 - dataLoadOptions - An object with parameters for pagination
-    //    * @param  {boolean} showLoader Boolean to determine if the page loader
-    //    * is active
-    //    * @param {function} callback Callback
-    //    */
+      /**
+       * Check if the view is showing a detail or the list. The function checks
+       * if $stateParams.detail is set.
+       * @return {boolean} True if the view must show a detail.
+       */
+      // isDetail: function() {
+      //   return $stateParams.detail !== "";
+      // },
+      /**
+       * Show the detail getting the index from $stateParams.detail. Set "item"
+       * to the selected detail
+       */
+      // showDetail: function(detailIndex) {
+      //   if (isDefined($stateParams.detail) && $stateParams.detail !== "") {
+      //     var itemIndex = _.findIndex($scope.items, function(item) {
+      //       return item.id.toString() === $stateParams.detail;
+      //     });
+      //     if (itemIndex === -1) {
+      //       dataLoadOptions = {
+      //         offset: $scope.items === undefined ? 0 : $scope.items.length,
+      //         items: 25,
+      //         cache: false
+      //       };
+      //       list.load(false, function() {
+      //         list.showDetail();
+      //       });
+      //     } else {
+      //       $scope.detail = $scope.items[itemIndex];
+      //     }
+      //   } else if (isDefined(detailIndex)) {
+      //     $scope.detail = $scope.items[detailIndex];
+      //   }
+      // },
+      /**
+       * Show the moblet main view
+       * @param {Object} data The data from the instance plus all API URLs
+       */
       showMainView: function(data) {
         var deferred = $q.defer();
         var finished = 0;
         var totalLoad = 2;
-        function finishedLoading() {
+
+        var finishedLoading = function() {
           finished += 1;
           if (finished === totalLoad) {
-            deferred.resolve();
+            deferred.resolve('ok');
           }
-        }
+        };
 
         if (isDefined(data)) {
           $scope.error = false;
@@ -220,14 +148,18 @@ module.exports = {
           model.getData(data.store)
             .then(function(response) {
               $scope.store = response.data;
+              $scope.categories = response.data.headerJson.headerCategoryJsons;
+              console.log($scope.store);
               finishedLoading();
             })
             .catch(function(error) {
               console.error(error);
             });
+
           model.getData(data.banner)
             .then(function(response) {
               $scope.banners = response.data;
+              console.log($scope.banners);
               finishedLoading();
             })
             .catch(function(error) {
@@ -265,12 +197,6 @@ module.exports = {
           });
       }
     };
-    // // var listItem = {
-    //   goTo: function(detail) {
-    //     $stateParams.detail = detail.id;
-    //     $state.go('pages', $stateParams);
-    //   }
-    // };
     controller.init();
   }
 };
