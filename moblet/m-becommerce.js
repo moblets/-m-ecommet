398ea0045bd88b82c9b13bac09f38e0b188b79b2
Eduardo Russo <russoedu@gmail.com>
2016-11-20 07:52:58 -0200

    [master] Routes corrected

--- moblet/m-becommerce.js
+++ moblet/m-becommerce.js
@@ -22,16 +22,23 @@ module.exports = {
     $http,
     $q
   ) {
-    var instanceData = {};
+    var page = {
+      STORE: 'store',
+      CATEGORY: 'category',
+      SUBCATEGORY: 'subcategory',
+      PRODUCT: 'product',
+      CART: 'cart'
+    };
     var model = {
-      dataLoadOptions: {
-        cache: ($stateParams.detail !== "")
-      },
-      setInstanceData: function() {
+      loadInstanceData: function() {
         var deferred = $q.defer();
-        $mDataLoader.load($scope.moblet, model.dataLoadOptions)
+        var dataLoadOptions = {
+          cache: false
+        };
+
+        $mDataLoader.load($scope.moblet, dataLoadOptions)
           .then(function(data) {
-            instanceData = {
+            $scope.instanceData = {
               apiUrl: data.apiUrl,
               clientUrl: data.clientUrl,
               store: data.apiUrl + 'common/conf/store/',
@@ -55,7 +62,7 @@ module.exports = {
                   zipCode + '/' + productId + '/1';
               }
             };
-            deferred.resolve(instanceData);
+            deferred.resolve();
           })
           .catch(function(error) {
             console.error(error);
@@ -77,28 +84,16 @@ module.exports = {
         );
         return deferred.promise;
       }
-    // getData: function(url, callback) {
-    //   $http.get(apiUrl + url)
-    //     .then(
-    //       function(response) {
-    //         callback(false, response);
-    //       },
-    //       function(error) {
-    //         console.log(error);
-    //         calback(true);
-    //       }
-    //     );
-    // }
     };
-    // var dataLoadOptions;
-    // var apiUrl = '';
-    var controller = {
+    var helpers = {
       localizeCurrency: function(value) {
         var corrected = $filter('currency')(value, "R$", 2);
         var splited = corrected.split('.');
         var localized = splited[0].replace(',', '.') + '.' + splited[1];
         return localized;
-      },
+      }
+    };
+    var storeController = {
       loadMoreFromSection: function(section) {
         section.limit = section.products.length;
         $scope.isLoading = true;
@@ -114,8 +109,7 @@ module.exports = {
         model.getData($scope.instanceData.detail(productId))
           .then(function(response) {
             console.log(response.data);
-            $stateParams.detail = 'product/' + productId;
-            $scope.view = 'product';
+            $stateParams.detail = page.PRODUCT + '&' + productId;
             $state.go('pages', $stateParams);
           })
           .catch(function(error) {
@@ -123,65 +117,42 @@ module.exports = {
           });
       },
       /**
-       * Check if the view is showing a detail or the list. The function checks
-       * if $stateParams.detail is set.
-       * @return {boolean} True if the view must show a detail.
-       */
-      // isDetail: function() {
-      //   return $stateParams.detail !== "";
-      // },
-      /**
-       * Show the detail getting the index from $stateParams.detail. Set "item"
-       * to the selected detail
-       */
-      // showDetail: function(detailIndex) {
-      //   if (isDefined($stateParams.detail) && $stateParams.detail !== "") {
-      //     var itemIndex = _.findIndex($scope.items, function(item) {
-      //       return item.id.toString() === $stateParams.detail;
-      //     });
-      //     if (itemIndex === -1) {
-      //       dataLoadOptions = {
-      //         offset: $scope.items === undefined ? 0 : $scope.items.length,
-      //         items: 25,
-      //         cache: false
-      //       };
-      //       list.load(false, function() {
-      //         list.showDetail();
-      //       });
-      //     } else {
-      //       $scope.detail = $scope.items[itemIndex];
-      //     }
-      //   } else if (isDefined(detailIndex)) {
-      //     $scope.detail = $scope.items[detailIndex];
-      //   }
-      // },
-      /**
        * Show the moblet main view
        * @param {Object} data The data from the instance plus all API URLs
        * @return {Promise} Show the store view when all needed data is loaded
        */
-      prepareStoreView: function() {
+      showView: function() {
         var deferred = $q.defer();
-        var finished = 0;
-        var totalLoad = 5;
-        var originalSectionsLimit = 4;
-        var sections = [];
-
-        var finishedLoading = function() {
-          finished += 1;
-          if (finished === totalLoad) {
-            $scope.view = 'store';
-            $scope.sections = sections;
-            $scope.loadMore = controller.loadMoreFromSection;
-            $scope.goToProduct = controller.goToProduct;
-
-            deferred.resolve();
-          }
-        };
 
         if (isDefined($scope.instanceData)) {
-          $scope.error = false;
-          $scope.emptyData = false;
+          $scope.sections = [];
+
+          var finished = 0;
+          var totalLoad = 5;
+          var sectionLimit = 4;
+
+          var finishedLoading = function() {
+            finished += 1;
+            if (finished === totalLoad) {
+              // Set the view
+              $scope.view = page.STORE;
+              // Set the STORE functions
+              $scope.loadMore = storeController.loadMoreFromSection;
+              $scope.goToProduct = storeController.goToProduct;
+              // Set error and emptData to false
+              $scope.error = false;
+              $scope.emptyData = false;
+
+              // Remove the loader
+              $scope.isLoading = false;
+
+              // Broadcast complete refresh and infinite scroll
+              $rootScope.$broadcast('scroll.refreshComplete');
+              $rootScope.$broadcast('scroll.infiniteScrollComplete');
+
+              deferred.resolve();
+            }
+          };
 
           model.getData($scope.instanceData.store)
             .then(function(response) {
@@ -203,9 +174,9 @@ module.exports = {
             });
           model.getData($scope.instanceData.sections.section3)
             .then(function(response) {
-              sections[0] = {
+              $scope.sections[0] = {
                 name: 'Vitrine 1',
-                limit: originalSectionsLimit,
+                limit: sectionLimit,
                 products: response.data
               };
               finishedLoading();
@@ -215,9 +186,9 @@ module.exports = {
             });
           model.getData($scope.instanceData.sections.section4)
             .then(function(response) {
-              sections[1] = {
+              $scope.sections[1] = {
                 name: 'Vitrine 2',
-                limit: originalSectionsLimit,
+                limit: sectionLimit,
                 products: response.data
               };
               finishedLoading();
@@ -227,9 +198,9 @@ module.exports = {
             });
           model.getData($scope.instanceData.sections.section5)
             .then(function(response) {
-              sections[2] = {
+              $scope.sections[2] = {
                 name: 'Vitrine 3',
-                limit: originalSectionsLimit,
+                limit: sectionLimit,
                 products: response.data
               };
               finishedLoading();
@@ -238,33 +209,33 @@ module.exports = {
               console.error(error);
             });
         } else {
+          $scope.isLoading = false;
+
           $scope.error = true;
           $scope.emptyData = true;
+
+          deferred.resolve();
         }
 
-        // Broadcast complete refresh and infinite scroll
-        $rootScope.$broadcast('scroll.refreshComplete');
-        $rootScope.$broadcast('scroll.infiniteScrollComplete');
         return deferred.promise;
-      },
-
+      }
+    };
+    var router = {
       init: function() {
+        console.debug('router.init()');
         // Set general status
         $scope.isLoading = true;
-        $scope.localizeCurrency = controller.localizeCurrency;
+        $scope.localizeCurrency = helpers.localizeCurrency;
         $scope.colors = $mAppDef().load().colors;
-        console.log($stateParams);
+        // Decide where to go based on the $stateParams
         if ($stateParams.detail === '') {
+          console.debug('STORE');
+          /** STORE PAGE **/
           // Load the appDef data
-          model.setInstanceData()
-            .then(function(data) {
-              // Save the instance in the scope
-              $scope.instanceData = data;
-              // Prepare the index (store view)
-              return controller.prepareStoreView();
-            })
+          model.loadInstanceData()
             .then(function() {
-              $scope.isLoading = false;
+              // Prepare the store view
+              return storeController.showView();
             })
             .catch(function(error) {
               console.error(error);
@@ -272,9 +243,23 @@ module.exports = {
               $scope.error = true;
             });
         } else {
-          var params = $stateParams.detail.split('/');
-          if (params[0] === 'product') {
-            console.log(params[1]);
+          var detail = $stateParams.detail.split('&');
+          $scope.view = detail[0];
+          $stateParams.detail = detail[1];
+          if ($scope.view === page.CATEGORY) {
+            /** PRODUCT PAGE **/
+            console.debug('CATEGORY');
+          } else if ($scope.view === page.SUBCATEGORY) {
+            /** CATEGORY PAGE **/
+            console.debug('SUBCATEGORY');
+          } else if ($scope.view === page.PRODUCT) {
+            /** SUBCATEGORY PAGE **/
+            console.debug('PRODUCT');
+            $scope.productId = $stateParams.detail;
+            $scope.isLoading = false;
+          } else if ($scope.view === page.CART) {
+            /** CART PAGE **/
+            console.debug('CART');
           }
         }
       }
@@ -283,6 +268,6 @@ module.exports = {
     window.malbumImageLoaded = function(element) {
       element.parentElement.classList.add("loaded");
     };
-    controller.init();
+    router.init();
   }
 };
